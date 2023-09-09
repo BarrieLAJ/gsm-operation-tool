916f9fb32c3d52a1353b421fe9965d573c7333e7
Alhaji Abubakarr Barrie <alhajiab.barrie@gmail.com>
2023-09-09 20:38:11 +0000

    Updates...

--- pages/vessel-details/index.tsx
+++ pages/vessel-details/index.tsx
@@ -4,152 +4,111 @@ import { authProvider } from "src/authProvider";
 import React from "react";
 
 import { useDataGrid, List, DateField } from "@refinedev/mui";
-import { GridColDef, GridRow, GridRowProps } from "@mui/x-data-grid/";
+import { GridColDef, GridRow, GridRowProps } from "@mui/x-data-grid";
 import { IResourceComponentsProps, useNavigation } from "@refinedev/core";
 import { Box, Button } from "@mui/material";
 import { Edit } from "@mui/icons-material";
 import dynamic from "next/dynamic";
-const DataGrid = dynamic(() => import("@components/commons/Table"), { ssr: false });
+// const DataGrid = dynamic(() => import("@components/commons/Table"), { ssr: false });
+
+const columns: GridColDef[] = [
+	{
+		field: "vessel_name",
+		flex: 1,
+		headerName: "Vessel Name",
+		minWidth: 200,
+	},
+	{
+		field: "eta",
+		flex: 1,
+		headerName: "ETA",
+		minWidth: 200,
+		renderCell: function render({ value }) {
+			return <DateField value={value} />;
+		},
+	},
+	{
+		field: "embark",
+		flex: 1,
+		headerName: "Embark",
+		type: "datetime",
+		headerAlign: "center",
+		align: "center",
+		minWidth: 200,
+		renderCell: function render({ value }) {
+			return <DateField value={value} />;
+		},
+	},
+	{
+		field: "disembark",
+		flex: 1,
+		headerName: "Disembark",
+		type: "datetime",
+		headerAlign: "center",
+		align: "center",
+		minWidth: 200,
+		renderCell: function render({ value }) {
+			return <DateField value={value} />;
+		},
+	},
+	{
+		field: "feeding_cost",
+		headerName: "Feeding Costs",
+		headerAlign: "center",
+		align: "center",
+		type: "number",
+		minWidth: 150,
+	},
+	{
+		field: "wages",
+		headerName: "Wages",
+		headerAlign: "center",
+		align: "center",
+		type: "number",
+		minWidth: 150,
+	},
+	{
+		field: "captain_name",
+		flex: 1,
+		headerName: "Captain Name",
+		minWidth: 200,
+	},
+	{
+		field: "actions",
+		headerName: "Actions",
+		type: "actions",
+		sortable: false,
+		renderCell: function render({ row }) {
+			const { show, edit } = useNavigation();
+			return (
+				<>
+					<Button
+						// hideText
+						// recordItemId={row.id}
+						onMouseDown={(e) => {
+							e.stopPropagation();
+						}}
+						onClick={(e) => {
+							edit("vessel-details", row.id);
+							e.stopPropagation();
+						}}
+					>
+						<Edit />
+					</Button>
+				</>
+			);
+		},
+		align: "center",
+		headerAlign: "center",
+		minWidth: 80,
+	},
+];
 
 export const VesselDetailList: React.FC<IResourceComponentsProps> = () => {
 	const { dataGridProps } = useDataGrid({});
-	const columns = React.useMemo<GridColDef[]>(
-		() => [
-			{
-				field: "vessel_name",
-				flex: 1,
-				headerName: "Vessel Name",
-				minWidth: 200,
-			},
-			{
-				field: "eta",
-				flex: 1,
-				headerName: "ETA",
-				// type: "date",
-				minWidth: 200,
-				renderCell: function render({ value }) {
-					return <DateField value={value} />;
-				},
-			},
-			{
-				field: "embark",
-				flex: 1,
-				headerName: "Embark",
-				type: "datetime",
-				headerAlign: "center",
-				align: "center",
-				minWidth: 200,
-				renderCell: function render({ value }) {
-					return <DateField value={value} />;
-				},
-			},
-			{
-				field: "disembark",
-				flex: 1,
-				headerName: "Disembark",
-				type: "datetime",
-				headerAlign: "center",
-				align: "center",
-				minWidth: 200,
-				renderCell: function render({ value }) {
-					return <DateField value={value} />;
-				},
-			},
-			{
-				field: "feeding_cost",
-				// flex: 1,
-				headerName: "Feeding Costs",
-				headerAlign: "center",
-				align: "center",
-				type: "number",
-				minWidth: 150,
-			},
-			{
-				field: "wages",
-				// flex: 1,
-				headerName: "Wages",
-				headerAlign: "center",
-				align: "center",
-				type: "number",
-				minWidth: 150,
-			},
-			// {
-			// 	field: "imo_number",
-			// 	flex: 1,
-			// 	headerName: "IMO Number",
-			// 	type: "string",
-			// 	minWidth: 200,
-			// },
-			// {
-			// 	field: "vessel_email",
-			// 	flex: 1,
-			// 	headerName: "Vessel Email",
-			// 	minWidth: 250,
-			// 	renderCell: function render({ value }) {
-			// 		return <EmailField value={value} />;
-			// 	},
-			// },
-			// {
-			// 	field: "vessel_owner_name",
-			// 	flex: 1,
-			// 	headerName: "Vessel Owner Name",
-			// 	minWidth: 250,
-			// 	renderCell: function render({ row }) {
-			// 		const value = row.vessel_owner;
-			// 		return <>{value?.name || ""}</>;
-			// 	},
-			// },
-			// {
-			// 	field: "vessel_owner",
-			// 	flex: 1,
-			// 	headerName: "Vessel Owner Email",
-			// 	minWidth: 250,
-			// 	renderCell: function render({ value }) {
-			// 		return <EmailField value={value?.email || ""} />;
-			// 	},
-			// },
-			{
-				field: "captain_name",
-				flex: 1,
-				headerName: "Captain Name",
-				minWidth: 200,
-			},
-			{
-				field: "actions",
-				headerName: "Actions",
-				type: "actions",
-				sortable: false,
-				renderCell: function render({ row }) {
-					const { show, edit } = useNavigation();
-					return (
-						<>
-							<Button
-								// hideText
-								// recordItemId={row.id}
-								onMouseDown={(e) => {
-									e.stopPropagation();
-								}}
-								onClick={(e) => {
-									edit("vessel-details", row.id);
-									e.stopPropagation();
-								}}
-							>
-								<Edit />
-							</Button>
-						</>
-					);
-				},
-				align: "center",
-				headerAlign: "center",
-				minWidth: 80,
-			},
-		],
-		[]
-	);
-
 	return (
 		<List>
-			<DataGrid
+			{/* <DataGrid
 				{...dataGridProps}
 				columns={columns}
 				autoHeight
@@ -183,7 +142,7 @@ export const VesselDetailList: React.FC<IResourceComponentsProps> = () => {
 						);
 					},
 				}}
-			/>
+			/> */}
 		</List>
 	);
 };
